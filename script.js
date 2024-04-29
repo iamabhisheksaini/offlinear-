36152e03156b36dc531b5350ddd7e9c34ab36608
Abhishek Saini <124495434+iamabhisheksaini@users.noreply.github.com>
2024-04-29 19:23:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+const elemArr = [navCloseBtn, overlay, navOpenBtn];
+
+for (let i = 0; i < elemArr.length; i++) {
+  elemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+/**
+ * toggle navbar & overlay when click any navbar-link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-navbar-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+
+
+
+
+/**
+ * header & go-top-btn active
+ * when window scroll down to 400px
+ */
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 700) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
+
+/**
+ * header & go-top-btn active
+ * when window scroll down to 400px
+ */
+
+const bottomContact = document.querySelector("[data-bottom-container]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 700) {
+    bottomContact.classList.remove("mobile-bottom-hidden");
+  } else {
+    bottomContact.classList.add("mobile-bottom-hidden");
+  }
+});
